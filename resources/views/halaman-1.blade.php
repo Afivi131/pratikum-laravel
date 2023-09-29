5329fcf7369b0660f5235df02f39e8332bcdf809
afivi <muhammadafiviasghar01@gmail.com>
2023-09-29 15:00:30 -0700

    first commit

---
+++ resources/views/halaman-1.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>halaman 1</title>
+</head>
+<body>
+    <h1>halaman 1</h1> 
+    return view('halaman-1');
+</body>
+</html>
